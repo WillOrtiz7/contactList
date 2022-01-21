639efaecf60918c1b2c90827c61611c5043dc375
WillOrtiz7 <willortiz407@gmail.com>
2022-01-21 13:13:38 -0500

    Reworked front end

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 // Selectors
-const contactInput = document.querySelector('.contact-input');
-const searchButton = document.querySelector('.search-button');
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggestionBox = searchWrapper.querySelector(".autocom-box");
 
 // Event Listeners
+inputBox.onkeyup = (event) => {
+  let userInput = event.target.value;
+  let emptyArray = [];
+  if (userInput) {
+    emptyArray = suggestions.filter((input) => {
+      // Filtering user input to return names in database which start with the input entered
+      return input.toLocaleLowerCase().startsWith(userInput.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((input) => {
+      return (input = "<li>" + input + "</li>");
+    });
+    console.log(emptyArray);
+    // Show autocomplete box
+    searchWrapper.classList.add("active");
+    showSuggestions(emptyArray);
+    let allList = suggestionBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++){
+      // Adding on click attribute to each contacts li
+      allList[i].setAttribute("onclick", "select(this)");
+    }
+  }else{
+    // Hide autocomplete box
+    searchWrapper.classList.remove("active");
+  }
+};
 
 // Functions
 
-function searchContact(){
-    let input = document.getElementById("input-text").value;
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName('contacts');
-    let listMod = document.getElementById("list")
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display = "none";
-        }
-        else if (input === ""){
-            listMod.style.display = "none";
-        }
-        else {
-            x[i].style.display = "list-item";
-            listMod.style.display = "list-item";                 
-        }
-    }
+// We will use this function to open the drop down showing the contacts info
+function select(element){
+  let selectUserData = element.textContent;
+  if (selectUserData != "No matching contact"){
+    inputBox.value = selectUserData;
+    searchWrapper.classList.remove("active");
+  }
+}
+
+function showSuggestions(list){
+  let listData;
+  if (!list.length){
+    listData = "<li>"+ "No matching contact" +"</li>"
+  }else{
+    listData = list.join('');
+  }
+  suggestionBox.innerHTML = listData;
 }
