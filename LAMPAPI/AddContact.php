125d5b9f4e23a8f7050eb9fcfede20e7d0a7d52c
Whimsickal <declanpcassidy1@gmail.com>
2022-01-24 16:22:54 -0500

    Created AddContact endpoint

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,55 @@
+<?php
+
+# Retrieves json input from user request
+$inputData = getInputData();
+
+# MySql Connection Parameters
+$host = "167.99.228.82";
+$dbUser = "TheBeast";
+$dbPass = "WeLoveCOP4331";
+$dbName = "COP4331";
+
+$connection = new mysqli($host, $dbUser, $dbPass, $dbName);
+if( $connection->connect_error )
+{
+	returnWithError( $connection->connect_error );
+}
+else
+{
+	$statement = $connection->prepare("INSERT into Contacts (FirstName,LastName,PhoneNumber,EmailAddress,UserID) VALUES(?,?,?,?,?)");
+	$statement->bind_param("sssss", $inputData['firstName'], $inputData['lastName'], $inputData['phoneNumber'], $inputData['emailAddress'], $inputData['userID']);
+	$statement->execute();
+
+	returnWithInfo($inputData['firstName'], $inputData['lastName']);
+
+	$statement->close();
+	$connection->close();
+}
+
+# Input json will be decoded and true means that the decoded string will be turned into an array
+function getInputData()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+# Creates and returns formatted string with empty user data and an error passed to this function
+function returnWithError( $error )
+{
+	$retValue = '{"Error":"' . $error . '"}';
+	sendResultInfoAsJson( $retValue );
+}
+
+# Creates and returns formatted string with specified user data from the database and an empty error field
+function returnWithInfo( $firstName, $lastName )
+{
+	$retValue = '{"Successfully registered contact":"' . $firstName . '":"' . $lastName . '"}';
+	sendResultInfoAsJson( $retValue );
+}
+
+?>
