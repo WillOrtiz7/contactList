78ebe1d6c815317e0560240e93572d1a204f5dfc
Whimsickal <declanpcassidy1@gmail.com>
2022-01-24 17:32:16 -0500

    Added RemoveContact endpoint

---
+++ LAMPAPI/RemoveContact.php
@@ -0,0 +1,62 @@
+<?php
+
+# Retrieves json input from user request
+$inputData = getInputData();
+
+# MySql Connection Parameters
+$host = "167.99.228.82";
+$dbUser = "TheBeast";
+$dbPass = "WeLoveCOP4331";
+$dbName = "COP4331";
+
+$connection = new mysqli($host, $dbUser, $dbPass, $dbName);
+if( $connection->connect_error )
+{
+	returnWithError( $connection->connect_error );
+}
+else
+{
+	$statement = $connection->prepare("DELETE FROM Contacts WHERE FirstName=? AND LastName=? AND PhoneNumber=? AND EmailAddress=? AND UserID=?");
+	$statement->bind_param("sssss", $inputData['firstName'], $inputData['lastName'], $inputData['phoneNumber'], $inputData['emailAddress'], $inputData['userId']);
+    $statement->execute();
+
+    if ($statement->affected_rows > 0)
+    {
+        returnWithInfo($inputData['firstName'], $inputData['lastName']);
+    }
+    else
+    {
+        returnWithError("Failed to delete contact");
+    }
+
+	$statement->close();
+	$connection->close();
+}
+
+# Input json will be decoded and true means that the decoded string will be turned into an array
+function getInputData()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+# Creates and returns formatted string with empty user data and an error passed to this function
+function returnWithError( $error )
+{
+	$retValue = '{"Error":"' . $error . '"}';
+	sendResultInfoAsJson( $retValue );
+}
+
+# Creates and returns formatted string with specified user data from the database and an empty error field
+function returnWithInfo( $firstName, $lastName )
+{
+	$retValue = '{"Successfully deleted contact":"' . $firstName . " " . $lastName . '"}';
+	sendResultInfoAsJson( $retValue );
+}
+
+?>
