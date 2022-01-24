65d930317a723007fe9ed1c11fa17efd52b8b0e0
Whimsickal <declanpcassidy1@gmail.com>
2022-01-24 17:41:40 -0500

    Added RemoveUser endpoint

---
+++ LAMPAPI/RemoveUser.php
@@ -0,0 +1,62 @@
+<?php
+
+# Retrieves json input from user request
+$inputData = getInputData();
+
+# MySql Connection Parameters
+$host = "167.99.228.82";
+$dbUser = "TheBeast";
+$dbPass = "WeLoveCOP4331";
+$dbName = "COP4331";
+
+$connection = new mysqli($host, $dbUser, $dbPass, $dbName);
+if( $connection->connect_error )
+{
+	returnWithError( $connection->connect_error );
+}
+else
+{
+	$statement = $connection->prepare("DELETE FROM Users WHERE FirstName=? AND LastName=? AND Login = ? AND Password = ?");
+	$statement->bind_param("ssss", $inputData['firstName'], $inputData['lastName'], $inputData['login'], $inputData['password']);
+    $statement->execute();
+
+    if ($statement->affected_rows > 0)
+    {
+        returnWithInfo($inputData['login']);
+    }
+    else
+    {
+        returnWithError("Failed to delete user");
+    }
+
+	$statement->close();
+	$connection->close();
+}
+
+# Input json will be decoded and true means that the decoded string will be turned into an array
+function getInputData()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+# Creates and returns formatted string with empty user data and an error passed to this function
+function returnWithError( $error )
+{
+	$retValue = '{"Error":"' . $error . '"}';
+	sendResultInfoAsJson( $retValue );
+}
+
+# Creates and returns formatted string with specified user data from the database and an empty error field
+function returnWithInfo( $login)
+{
+	$retValue = '{"Successfully deleted user":"' . $login . '"}';
+	sendResultInfoAsJson( $retValue );
+}
+
+?>
