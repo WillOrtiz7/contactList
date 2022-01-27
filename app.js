186572a3dd01e2a47e87b2fb5967e53917a7faf0
WillOrtiz7 <willortiz407@gmail.com>
2022-01-27 10:45:26 -0500

    Added structure for search function

--- app.js
+++ app.js
@@ -1,118 +1,103 @@
-// Selectors
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggestionBox = searchWrapper.querySelector(".autocom-box");
+// SELECTORS
+const searchBar = document.getElementById("search-input");
 const urlBase = "http://cop4331-27.com/LAMPAPI";
-const ext = ".php"
+const ext = ".php";
 
-// Event Listeners
-inputBox.onkeyup = (event) => {
-  let userInput = event.target.value;
-  let emptyArray = [];
-  if (userInput) {
-    emptyArray = suggestions.filter((input) => {
-      // Filtering user input to return names in database which start with the input entered
-      return input.toLocaleLowerCase().startsWith(userInput.toLocaleLowerCase());
-    });
-    emptyArray = emptyArray.map((input) => {
-      return (input = "<li>" + input + "</li>");
-    });
-    console.log(emptyArray);
-    // Show autocomplete box
-    searchWrapper.classList.add("active");
-    showSuggestions(emptyArray);
-    let allList = suggestionBox.querySelectorAll("li");
-    for (let i = 0; i < allList.length; i++){
-      // Adding on click attribute to each contacts li
-      allList[i].setAttribute("onclick", "select(this)");
-    }
-  }else{
-    // Hide autocomplete box
-    searchWrapper.classList.remove("active");
-  }
-};
+// OTHER VARIABLES
+let contactNames = [];
 
-document.addEventListener("click", event =>{
+// EVENT LISTENERS
+
+// Adds an event listener to the search bar which triggers whenver the user types
+searchBar.addEventListener("keyup", (event) => {
+  const searchString = event.target.value;
+  console.log(event.target.value);
+  const filteredContacts = contactNames.filter((contact) => {
+    return contact.name.includes(searchString);
+  });
+  console.log(filteredContacts);
+});
+
+document.addEventListener("click", (event) => {
   const isButton = event.target.matches(".login");
 
-  if(!isButton && event.target.closest(".login-dropdown") !=null)
-    return;
-  
+  if (!isButton && event.target.closest(".login-dropdown") != null) return;
+
   let currButton;
 
-  if(isButton){
+  if (isButton) {
     currButton = event.target.closest(".login-dropdown");
     currButton.classList.toggle("active");
   }
 
-  document.querySelectorAll(".login-dropdown.active").forEach(button =>{
-    if (button === currButton) 
-      return;
+  document.querySelectorAll(".login-dropdown.active").forEach((button) => {
+    if (button === currButton) return;
     button.classList.remove("active");
-  })
-})
+  });
+});
 
 document.onkeydown = (event) => {
-  if (event.key == 'Enter'){
+  if (event.key == "Enter") {
     let currAction = event.target.closest(".login-menu");
-    
-    if(currAction == document.getElementById("register")){
+
+    if (currAction == document.getElementById("register")) {
       executeRegister();
       event.preventDefault();
     }
   }
-}
+};
 
-// Functions
+// FUNCTIONS
 
 // We will use this function to open the drop down showing the contacts info
-function select(element){
+function select(element) {
   let selectUserData = element.textContent;
-  if (selectUserData != "No matching contact"){
+  if (selectUserData != "No matching contact") {
     inputBox.value = selectUserData;
     searchWrapper.classList.remove("active");
   }
   searchWrapper.classList.remove("active");
 }
 
-function executeRegister(){
+function executeRegister() {
   let firstName = document.getElementById("first-name").value;
   let lastName = document.getElementById("last-name").value;
   let user = document.getElementById("desired-user").value;
   let pass = document.getElementById("desired-password").value;
 
-  let obj = {login:user, password:pass, firstName:firstName, lastName:lastName};
+  let obj = {
+    login: user,
+    password: pass,
+    firstName: firstName,
+    lastName: lastName,
+  };
   obj = JSON.stringify(obj);
 
   let link = new XMLHttpRequest();
-  let requestUrl = urlBase + '/Register.php';
+  let requestUrl = urlBase + "/Register.php";
   link.open("POST", requestUrl, true);
   link.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  link.setRequestHeader("Access-Control-Allow-Origin" , urlBase);
-  link.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) 
-			{
-				let response = JSON.parse( link.responseText );
-        if(response.error.length != 0)
-        {
-          console.log(response.error);
-        }
-        else
-        {
-          console.log("Successfully registered");
-        }
+  link.setRequestHeader("Access-Control-Allow-Origin", urlBase);
+  link.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let response = JSON.parse(link.responseText);
+      if (response.error.length != 0) {
+        console.log(response.error);
+      } else {
+        console.log("Successfully registered");
       }
-  }
+    }
+  };
   link.send(obj);
   console.log(firstName, lastName, user, pass);
 }
 
-function showSuggestions(list){
+function showSuggestions(list) {
   let listData;
-  if (!list.length){
-    listData = "<li>"+ "No matching contact" +"</li>"
-  }else{
-    listData = list.join('');
+  if (!list.length) {
+    listData = "<li>" + "No matching contact" + "</li>";
+  } else {
+    listData = list.join("");
   }
   suggestionBox.innerHTML = listData;
 }
